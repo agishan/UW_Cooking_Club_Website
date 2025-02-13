6bd0ff380faf2d946104e6298b8e15959721145b
Brandon Kong <brandonkong@rogers.com>
2025-02-13 16:29:36 -0500

    models updated

--- UWCookingWebsite/CookingApp/models.py
+++ UWCookingWebsite/CookingApp/models.py
@@ -4,10 +4,23 @@ from django.db import models
 
 class Recipe(models.Model):
     name = models.CharField(max_length=200)
-    ingredients = models.CharField(max_length=200)
-    procedure = models.CharField(max_length=1000)
-    cuisine = models.CharField(max_length=100)
-    time = models.CharField(max_length=100)
-    meal_type = models.CharField(max_length=100)
     images = models.ImageField(default='',upload_to='Recipe_Images/')
+    cooking_class = models.CharField(max_length=200,default='')
+    description = models.TextField(default='')
+    difficulty = models.DecimalField(max_digits=2,decimal_places=1,default=0.0)
+    prep_time = models.CharField(max_length=200,default='')
+    cook_time = models.CharField(max_length=200,default='')
+    total_time = models.CharField(max_length=200,default='')
+    serving_size = models.DecimalField(max_digits=2,decimal_places=1,default=0.0)
+    equipment = models.TextField(default='')
+    ingredients = models.TextField(default='')
+    procedure = models.TextField(default='')
+
+class cooking_class(models.Model):
+    images = models.ImageField(default='',upload_to='Cooking_Class_Images/')
+    date = models.DateField()
+    instructor = models.CharField(max_length=200,default='')
+    recipes = models.TextField(default='')
+
+
 
