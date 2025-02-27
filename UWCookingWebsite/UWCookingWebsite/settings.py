4004382442dc17cd8ef6979ce10a37b313db3bd2
Agishan <54005039+agishan@users.noreply.github.com>
2025-02-26 19:28:24 -0500

    Changed routing path for images

--- UWCookingWebsite/UWCookingWebsite/settings.py
+++ UWCookingWebsite/UWCookingWebsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +28,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-MEDIA_ROOT = "C:\\Users\\Brandon\\Documents\\GitHub\\UW_Cooking_Club_Website\\UWCookingWebsite\\Media"
+
+MEDIA_URL = '/media/'  # This defines the URL path for media files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # This is where media files are stored
 
 # Application definition
 
